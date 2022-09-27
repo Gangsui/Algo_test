2e786cddf6e01f6304102393494f69d332f263d2
Yunseo <ppkvkr2@gmail.com>
2022-09-28 08:56:09 +0900

    실패율최종

--- Algorithm Answers/Fail_percentage.py
+++ Algorithm Answers/Fail_percentage.py
@@ -1,52 +1,17 @@
-from multiprocessing.sharedctypes import Value
-from re import A
-
-
+# 실패율문제 시간 초과
 def solution(N, stages):
-    answer = [] 
     N_dic={}
     a=len(stages)
-    to_answer=[]
-    for nn in range(N+1):
-        N_dic[nn+1]=0
     
-    for stage in stages:       
-        N_dic[stage]=N_dic[stage]+1
+    for nn in range(1,N+1):
+        if a !=0:
+            Coun_stag=stages.count(nn)
+            N_dic[nn]=Coun_stag/a
+            a-=Coun_stag
+        else:
+            N_dic[nn]=0
+    
+    
+    return sorted(N_dic,key=lambda x:N_dic[x],reverse=True)
 
-    for aa in range(1,N+1):
-        b=float(N_dic[aa])/a
-        a-=N_dic[aa]
-        N_dic[aa]=b
-
-        answer.append(b)
-    answer.sort()
-    answer.reverse()
-    K_V=[]
-    
-    
-
-    for ss in range(N):
-        key,value=N_dic.items()
-        K_V.append([key,value])
-    print(K_V)
-    for aa in answer:
-        for K in K_V:
-            if aa==K[1]:
-                to_answer.append(K[0])
-                K[1]=-1
-    
-   
-        
-    
-
-    
-    
-    
-    
-        print(to_answer)
-    return answer
-
-
-
-
-solution(4, [4, 4, 4, 4, 4])
+print(solution(5,[2, 1, 2, 6, 2, 4, 3, 3]	))
